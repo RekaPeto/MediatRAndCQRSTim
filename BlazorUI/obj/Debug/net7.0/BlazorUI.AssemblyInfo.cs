d96c270bfd25444098bbd54ce6c937be88d0c081
Peto Reka <peto.reka0@gmail.com>
2023-12-17 20:31:01 +0100

    Add api and needed classes to initialize GetPersonByID

--- BlazorUI/obj/Debug/net7.0/BlazorUI.AssemblyInfo.cs
+++ BlazorUI/obj/Debug/net7.0/BlazorUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3719f26a893ff20077c1aab0c1feb56271af1249")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
